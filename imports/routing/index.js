dde632d856df11efa5020a0da6fbfb38c22c7cfd
Justice Appiah <jappiah54@gmail.com>
2018-09-02 16:29:28 +0000

    Removed all .idea and .meteor files from repository and untracked them

--- imports/routing/index.js
+++ imports/routing/index.js
@@ -5,6 +5,9 @@ import Register from '/imports/ui/page/user/Register';
 import PostAdd from '/imports/ui/page/post/PostAdd';
 import PostList from '/imports/ui/page/post/PostList';
 import PostEdit from '/imports/ui/page/post/PostEdit';
+import CommentList from '/imports/ui/page/comment/CommentList';
+import CommentAdd from '/imports/ui/page/comment/CommentAdd';
+import CommentEdit from '/imports/ui/page/comment/CommentEdit';
 
 route('/', Home);
 
@@ -14,3 +17,7 @@ route('/register', Register);
 route('/posts/add', PostAdd);
 route('/posts', PostList);
 route('/posts/edit/:_id', PostEdit);
+
+route('/posts/:postId/comments', CommentList);
+route('/posts/:postId/comments/add', CommentAdd);
+route('/posts/:postId/comments/:commentId/edit', CommentEdit);
