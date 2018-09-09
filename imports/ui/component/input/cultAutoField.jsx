b9772e04f3548cd030b94b267d7c6461a1277b66
Justice Appiah <jappiah54@gmail.com>
2018-09-09 20:56:30 +0000

    Refactored CultAutoField component to use an internal scss config

--- imports/ui/component/input/cultAutoField.jsx
+++ imports/ui/component/input/cultAutoField.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { AutoField } from 'uniforms-unstyled';
 
+import './cultAutoField.scss'
+
 export default class CultAutoField extends Component {
 	
 	render() {
