8d03d757fae8f1f716367486ca406b3978227375
Justice Appiah <jappiah54@gmail.com>
2018-09-09 21:39:39 +0000

    Refactored button sass into mixins and extensions

--- imports/ui/component/button/button.jsx
+++ imports/ui/component/button/button.jsx
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 import './button.scss';
 
 export default Button = props =>
-	<button className="coc-button coc-button-cult" { ... props }>
+	<button className="coc-button-cult" { ... props }>
 		{ props.children }
 	</button>
