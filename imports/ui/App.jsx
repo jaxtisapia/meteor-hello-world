b14b67f3458803e94da32c81bd376c442fbf21f1
Justice Appiah <jappiah54@gmail.com>
2018-09-21 09:43:12 +0000

    made my first graphql query addition. YIPEEE

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,35 @@
+import gql from 'graphql-tag';
+
+import { initialize } from 'meteor/cultofcoders:apollo';
 import { withTracker } from 'meteor/react-meteor-data';
 import React from 'react';
 
+import { ApolloProvider, Query } from 'react-apollo';
+
+const { client } = initialize();
+
+const QUERY = gql`query { sayMasa }`;
+
 const App = ({ main, routeProps, user }) => {
 	
-	return (<div id="app">
-			<div className="uk-flex uk-flex-center uk-flex-column">
-				
-				{ React.createElement(main, routeProps, user) }
-			
+	return (
+		<ApolloProvider client={ client }>
+			<div id="app">
+				<div className="uk-flex uk-flex-center uk-flex-column">
+					
+					<Query query={ QUERY }>
+						{ ({ loading, error, data }) => {
+							if ( loading ) return <p>Loading...</p>;
+							if ( error ) return <p>Error :(</p>;
+							
+							return <p>{ data.sayMasa }</p>
+						} }
+					</Query>
+					
+					{ React.createElement(main, routeProps, user) }
+				</div>
 			</div>
-		</div>
+		</ApolloProvider>
 	)
 };
 
