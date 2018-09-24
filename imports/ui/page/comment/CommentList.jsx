15c0c2c7d19a49c8646939fb326c4b6f527932ae
Justice Appiah <jappiah54@gmail.com>
2018-09-24 19:58:08 +0000

    rearranged and minimised code in CommentList.jsx

--- imports/ui/page/comment/CommentList.jsx
+++ imports/ui/page/comment/CommentList.jsx
@@ -3,6 +3,9 @@ import CommentListQuery from '/imports/ui/view/CommentListQuery';
 import { withTracker } from 'meteor/react-meteor-data';
 import React, { Component } from 'react';
 
+const LoadingView = <p>Loading...</p>;
+const ErrorView = <p>Error :(</p>;
+const EmptyDataView = <p>No Comments for this Post</p>;
 
 class CommentList extends Component {
 	handleCreateCommentRedirection = () => Redirect.toAddComment(this.props.postId);
@@ -17,22 +20,17 @@ class CommentList extends Component {
 		
 		const { postId } = this.props;
 		
-		const LoadingView = <p>Loading...</p>;
-		const ErrorView = <p>Error :(</p>;
-		const EmptyDataView = <p>No Comments for this Post</p>;
+		const commentListQueryProps = {
+			postId,
+			loadingView : LoadingView,
+			errorView : ErrorView,
+			emptyDataView : EmptyDataView
+		};
 		
 		return (
 			<div>
 				
-				{ /*
-				 TODO: pass for instance, <CommentList> item to QueryCommentList,
-				 instead of explicitly using <Comment> within the QueryCommentList class
-				 */ }
-				
-				<CommentListQuery postId={ postId }
-				                  loadingView={ LoadingView }
-				                  errorView={ ErrorView }
-				                  emptyDataView={ EmptyDataView }/>
+				<CommentListQuery { ... commentListQueryProps }/>
 				
 				<button onClick={ this.handleCreateCommentRedirection }>
 					Create a new comment
