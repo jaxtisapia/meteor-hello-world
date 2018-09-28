fb577875667b747fc1753197e478c57659cdae07
Justice Appiah <jappiah54@gmail.com>
2018-09-28 20:15:49 +0000

    organised fonts and styles in assets folder

--- imports/routing/router.js
+++ imports/routing/router.js
@@ -1,6 +1,6 @@
 import App from '/imports/ui/App.jsx'; // or the place where you have your main entry component
-import '/imports/ui/styles/main.scss'
-import '/imports/ui/styles/uikit/uikit.min.css'
+import '/imports/ui/assets/styles/main.scss'
+import '/imports/ui/assets/styles/vendor/uikit/uikit.min.css'
 import { createRouter } from 'meteor/cultofcoders:meteor-react-routing';
 
 export default createRouter(App); // App is the main entry component for your routes
