0c9b08643bc2615e231cb0e7d17a95f1d1c7994e
Justice Appiah <jappiah54@gmail.com>
2018-09-28 20:50:33 +0000

    appollo-fied deletion comments

--- imports/ui/component/comment/comment.jsx
+++ imports/ui/component/comment/comment.jsx
@@ -1,5 +1,8 @@
+import { CommentQueries } from '/imports/constant/queries'
 import { CommentResource, Redirect } from '/imports/ui/util/service/index';
+import gql from 'graphql-tag';
 import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
 
 export default class Comment extends Component {
 	
@@ -13,14 +16,32 @@ export default class Comment extends Component {
 		const { post } = comment;
 		
 		const postId = post._id;
+		const commentId = comment._id;
+		
+		const { deleteComment } = CommentQueries;
+		const deleteCommentMutation = gql(deleteComment);
+		
 		
 		return (<div key={ comment._id }>
-			<p>Comment id: { comment._id } </p>
-			<p>Comment title: { comment.title }, Comment Description: { comment.description } </p>
-			<button onClick={ () => Redirect.toEditComment(postId, comment._id) }> Edit comment
-			</button>
-			<button onClick={ () => this.handleDeleteComment(comment._id) }> Delete comment</button>
-		</div>)
+				<p>Comment id: { comment._id } </p>
+				<p>Comment title: { comment.title }, Comment Description: { comment.description } </p>
+				<button onClick={ () => Redirect.toEditComment(postId, comment._id) }> Edit comment
+				</button>
+				
+				<Mutation mutation={ deleteCommentMutation }
+				          onError={ () => {alert('an error occurred')} }
+				          onCompleted={ () => {
+					          alert('comment Deleted');
+					          Redirect.toPosts()
+				          } }>
+					{ deleteComment =>
+						<button onClick={ () => deleteComment({ variables : { commentId } }) }> Delete comment</button>
+					}
+				
+				</Mutation>
+			
+			</div>
+		)
 	}
 	
 	handleDeleteComment(commentId) {
