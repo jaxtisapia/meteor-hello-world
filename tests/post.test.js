06143b7bf841074f2a43ecc71aa05742b18798d8
Justice Appiah <jappiah54@gmail.com>
2018-09-29 15:55:04 +0000

    all posts.tests passing now

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,50 +1,62 @@
-import { expect, assert } from 'chai';
-import Post from '../imports/api/resource/post/collection';
-import { PostService } from '../imports/services';
+import { assert, expect } from 'chai';
 
 import faker from 'faker';
+import { Posts, Users } from '../db';
+import { PostService, UserService } from '../imports/services';
 
-describe('Post', function() {
+describe('Posts', function() {
 	
 	let post;
 	
-	const userId = 'defaultUser';
 	const description = faker.internet.email();
 	const title = faker.name.findName();
 	
 	const invalidPostId = 'somewrongids';
-	const validPostId = '37PruwNv4c39fZJXk';
+	let validPostId = '37PruwNv4c39fZJXk';
+	let validUserId = 'someValidUserId';
 	
-	const PostValidator = Post.simpleSchema().namedContext();
+	const PostValidator = Posts.simpleSchema().namedContext();
 	
 	before(function() {
 		
-		// create a user
+		// Create a random User for the test
+		const email = `${faker.internet.email()}@email.com`;
+		const password = 'mySecurePassword';
 		
-		post = { title, userId };
+		validUserId = UserService.create({ email, password });
+		
+		// Create a random Post for the test
+		post = { title, userId : validUserId };
 	});
 	
 	after(function() {
-		// clear the database
-		if (Meteor.isDevelopment) Post.remove({});
+		// This space runs only in test mode.
+		// We are sure, it wouldnt affect any development servers
+		
+		// Clear the database to avoid bloating.
+		// Only Call Clearance of Collections that have been imported
+		if ( Meteor.isDevelopment ) {
+			Posts.remove({});
+			Users.remove({});
+		}
 	});
 	
 	describe('Model Validation', function() {
 		
-		it('should be able to model a Post object with at least userId and title', function() {
+		it('should be able to model a Posts object with at least userId and title', function() {
 			
 			const isValid = PostValidator.validate(post);
 			expect(isValid).to.be.true;
 		});
 		
-		it('should be unable to model a Post object without a title', function() {
-			const post = { description, userId };
+		it('should be unable to model a Posts object without a title', function() {
+			const post = { description, userId : validUserId };
 			const isValidPost = PostValidator.validate(post);
 			
 			expect(isValidPost).to.be.false;
 		});
 		
-		it('should be unable to model a Post object without a userId', function() {
+		it('should be unable to model a Posts object without a userId', function() {
 			const post = { description, title };
 			const isValidPost = PostValidator.validate(post);
 			
@@ -55,8 +67,9 @@ describe('Post', function() {
 	
 	describe('#create', function() {
 		
-		it('should be create with a user attached', function() {
+		it('should be able to create with a user attached', function() {
 			const postId = PostService.create(post);
+			validPostId = postId;
 			
 			expect(postId).to.not.equal(null);
 			expect(postId).to.be.a('string');
@@ -64,7 +77,7 @@ describe('Post', function() {
 		
 	});
 	
-	describe('#read', function()  {
+	describe('#read', function() {
 		
 		describe('get', function() {
 			
@@ -84,7 +97,7 @@ describe('Post', function() {
 		
 		describe('getById', function() {
 			
-			it('should return an empty object when no Post Id is specified', function() {
+			it('should return an empty object when no Posts Id is specified', function() {
 				const postWithEmptyId = PostService.getById();
 				expect(postWithEmptyId).to.be.an('object').that.is.empty;
 			});
@@ -94,55 +107,67 @@ describe('Post', function() {
 				expect(postWithInvalidId).to.be.an('object').that.is.empty;
 			});
 			
-			it('should return a valid Post when a valid Id is specified', function() {
+			it('should return a valid Posts when a valid Id is specified', function(done) {
 				
 				const postWithValidId = PostService.getById(validPostId);
 				
 				expect(postWithValidId._id).to.be.a('string');
 				expect(postWithValidId._id).to.equal(validPostId);
 				expect(postWithValidId.title).to.be.a('string');
+				
+				done();
 			});
 			
 		});
 		
 	});
 	
-	describe('#update', function()  {
+	describe('#update', function() {
 		
-		it('should throw an error when an update is attempted on no Post Id', function() {
-			expect(PostService.updateByID()).to.throw();
+		it('should throw an error when an update is attempted on no Posts Id', function(done) {
+			try {
+				PostService.updateByID();
+			}
+			catch ( e ) {
+				done()
+			}
 		});
 		
-		it('should throw an error when an invalid Post Id is specified', function() {
+		it('should throw an error when an invalid Posts Id is specified', function(done) {
 			
 			const updatedPostWithInvalidId = PostService.updateByID;
-			assert.throws(updatedPostWithInvalidId(userId, invalidPostId, {}));
+			
+			try {
+				updatedPostWithInvalidId(validUserId, invalidPostId, {});
+			}
+			catch ( e ) {
+				done()
+			}
+			
 		});
 		
-		it('should update a Post when valid Id supplied, but only one property specified', function() {
+		it('should update a Posts when valid Id supplied, but only one property specified', function() {
 			
 			const titleUpdate = { title : 'Updated Title: Here we go' };
-			const updatedPost = PostService.updateByID(userId, validPostId, titleUpdate);
+			PostService.updateByID(validUserId, validPostId, titleUpdate);
 			
-			console.log(updatedPost);
-			
+			const updatedPost = PostService.getById(validPostId);
 			expect(updatedPost.title).to.equal(titleUpdate.title);
 		});
 		
-		it('should update a Post when valid Id supplied, and more than one properties specified', function() {
+		it('should update a Posts when valid Id supplied, and more than one properties specified', function() {
 			
 			const titleAndDescriptionUpdate = {
 				title : 'Updated Title: Here we go',
 				description : 'Another one! One Hundred'
 			};
 			
-			const updatedPost = PostService.updateByID(userId, validPostId, titleAndDescriptionUpdate);
+			PostService.updateByID(validUserId, validPostId, titleAndDescriptionUpdate);
 			
-			console.log(updatedPost);
+			const updatedPost = PostService.getById(validPostId);
 			
 			expect(updatedPost.title).to.equal(titleAndDescriptionUpdate.title);
 			expect(updatedPost.description).to.equal(titleAndDescriptionUpdate.description);
-			
 		});
 		
 	});
@@ -152,27 +177,34 @@ describe('Post', function() {
 		const validID = 'somevalidID';
 		const invalidId = 'someinvalidId';
 		
-		it('should throw an error when a delete is attempted with no Post Id', function() {
+		it('should throw an error when a delete is attempted with no Posts Id', function(done) {
 			
-			const deletedPostWithNoId = PostService.deleteByID();
+			try {
+				PostService.deleteByID();
+			} catch ( e ) {
+				done()
+			}
 			
-			expect(deletedPostWithNoId).to.throw();
 		});
 		
-		it('should throw an error when an invalid Post Id is specified', function() {
+		it('should throw an error when an invalid Posts Id is specified', function(done) {
 			
-			const deletedPostWithInvalidId = PostService.deleteByID(invalidId, {});
+			try {
+				PostService.deleteByID(invalidId, {});
+			}
+			catch ( e ) {
+				done();
+			}
 			
-			expect(deletedPostWithInvalidId).to.throw();
 		});
 		
 		it('should update a Post when valid Id supplied, but only one property specified', function() {
 			
-			PostService.deleteByID(userId, validID);
+			PostService.deleteByID(validUserId, validPostId);
 			
-			const deletedPost = PostService.getById(validID);
+			const deletedPost = PostService.getById(validPostId);
 			
-			expect(deletedPost._id).to.not.exist();
+			expect(deletedPost).to.be.empty;
 		});
 		
 		
